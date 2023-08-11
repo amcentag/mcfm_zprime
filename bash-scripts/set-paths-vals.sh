8edea308f1de8e05e3637704427edcf2642f6ec4
Andrew McEntaggart <amcentaggart@gatech.edu>
2023-08-10 23:18:31 -0400

    update mass, width, renscale and facscale
    update mass, width in mdata.f depending on contrib. also set renscale and facscale to 10 GeV

--- bash-scripts/set-paths-vals.sh
+++ bash-scripts/set-paths-vals.sh
@@ -14,7 +14,7 @@ JOBPATH0='/home/mcentag/Documents/mcfm_zprime/jobs'
 # 0: pp -> gamma -> e+e-
 # 1: pp -> gamma/Z -> e+e-
 # 2: pp -> gamma/Z' -> e+e-
-channel='0'
+channel='1'
 
 ### edit inital values for input_Z.ini here ###
 # sqrts in TeV
@@ -38,6 +38,18 @@ sed -i "11s/.*/s='${sqrts}'/" runscript.sh
 cd $JOBPATH0
 sed -i "8s/.*/cd ${MCFMBINPATH}/" example.job
 
+# set contrib variable in couplz.f
 cd $MCFMBINPATH0
 sed -i "37s/.*/      contrib = $channel/" ../src/Need/couplz.f
+# set z or zp mass and width
+# if selecting photon contribution, it does not matter since coupling is zero
+if [ $channel -eq 1 ]
+then
+    sed -i "49s/.*/      data zmass_inp \/ 91.1876_dp \//" ../src/User/mdata.f
+    sed -i "88s/.*/      data zwidth \/ 2.4952_dp \//" ../src/User/mdata.f
+elif [ $channel -eq 2 ]
+then
+    sed -i "49s/.*/      data zmass_inp \/ 10000_dp \//" ../src/User/mdata.f
+    sed -i "88s/.*/      data zwidth \/ 240.17_dp \//" ../src/User/mdata.f
+fi
 make
